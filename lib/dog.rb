3dedf830eeb3409af797790daf8064cb403f6126
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-08 17:19:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,7 @@
 class Dog
-  def initialize(name, breed)
-    @name = name 
+  def initialize(name, breed = "Mut")
+    @name = name
     @breed = breed
   end
-  
-  def name(full)
-   name, breed = full.split
-   @name =name 
-   @breed = breed
-  end 
-  
-  def name 
-     "#{@name} #{@breed}".strip
-  end 
-  
 end
-
-mutt = Dog.new 
-mutt.name=(:@name, "Mutt")
+fido = Dog.new("Fido")
